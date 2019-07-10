09f69f8e55c424165bc0fa0b67e7e90925214817
Mutian Wang <mokuteno@gmail.com>
2019-07-10 00:47:55 -0700

    Parse more data and refactor

--- src/parser/htmlParser.go
+++ src/parser/htmlParser.go
@@ -34,14 +34,47 @@ Result Data Srruct
 	}
 ]
 */
+
+const (
+
+	CARD_DELIVERED_SLN = 296
+	CARD_DELIVERED_ELN = 496
+
+	CASE_RECEIVED_SLN = 1216
+	CASE_RECEIVED_ELN = 1416
+
+	CASE_ID_SLN = 1494
+	CASE_ID_ELN = 1694
+
+)
+
+/**
+ * @param {string}
+ */
 func ParseCheckUSCIS(rawHTML string) {
-	cardWasMailedCnt := strings.Split(rawHTML, "\n")[296:496]
+	caseIDs := strings.Split(rawHTML, "\n")[CASE_ID_SLN:CASE_ID_ELN]
+	cardDeliveredCnt := strings.Split(rawHTML, "\n")[CARD_DELIVERED_SLN:CARD_DELIVERED_ELN]
+	caseReceivedCnt := strings.Split(rawHTML, "\n")[CASE_RECEIVED_SLN:CASE_RECEIVED_ELN]
+
 	// Stripe spaces and comma
-	for idx, ln := range cardWasMailedCnt {
+	fmtJSONLineToArr(caseIDs)
+	fmtJSONLineToArr(cardDeliveredCnt)
+	fmtJSONLineToArr(caseReceivedCnt)
+	
+	fmt.Println(caseIDs)
+	fmt.Println(cardDeliveredCnt)
+	fmt.Println(caseReceivedCnt)
+
+
+}
+
+/**
+ * @param  {[[]string]} Lines in string fromat, contains comma and/or spaces
+ */
+func fmtJSONLineToArr(dataLines []string){
+	for idx, ln := range dataLines {
 		s := strings.TrimSpace(ln)
 		s = strings.Replace(s, ",", "", -1)
-		cardWasMailedCnt[idx] = s
-	} 
-	fmt.Println(cardWasMailedCnt)
-
+		dataLines[idx] = s
+	}
 }
